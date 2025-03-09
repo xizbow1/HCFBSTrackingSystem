a280cafd9c15eda897c2866cd922e34337b76bc5
Gwillis442 <122920316+Gwillis442@users.noreply.github.com>
2025-03-09 13:47:00 -0700

    user id now set to session_variable

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { signup, login } from '../controllers/authController.js';
+import * as authController from '../controllers/authController.js';
 import { uploadApplicantFiles } from '../middlewares/uploadMiddleware.js';
 
 const router = express.Router();
 
 // Auth routes
-router.post('/signup', uploadApplicantFiles, signup);
-router.post('/login', login);
+router.post('/signup', uploadApplicantFiles, authController.signup);
+router.post('/login', authController.login);
 
 export default router;
